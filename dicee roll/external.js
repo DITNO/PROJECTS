6dc53ff288df5d287f78fe13bd1cd256e061f0c2
DITNO <92884446+DITNO@users.noreply.github.com>
2023-04-14 00:58:09 +0530

    Update external.js

--- dicee roll/external.js
+++ dicee roll/external.js
@@ -2,3 +2,7 @@ var randomNumber1 = Math.floor(Math.random()*6)+1;
 var randomDiceimg1 = "./images/dice"+randomNumber1+".png";
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomDiceimg1);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomDiceimg2 = "./images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceimg2);
