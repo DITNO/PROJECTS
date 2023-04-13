f608843a11286e5708e7cb67f93827f60d71b9e6
DITNO <92884446+DITNO@users.noreply.github.com>
2023-04-13 23:37:21 +0530

    Create external.js

---
+++ dicee roll/external.js
@@ -0,0 +1,9 @@
+var randomNumber1 = Math.floor((Math.random()*6)+1);
+console.log(randomNumber1);
+document.querySelector("img").getAttribute("img");
+document.querySelector("img").setAttribute("img","./images/dice1.png");
+document.querySelector("img").setAttribute("img","./images/dice2.png");
+document.querySelector("img").setAttribute("img","./images/dice3.png");
+document.querySelector("img").setAttribute("img","./images/dice4.png");
+document.querySelector("img").setAttribute("img","./images/dice5.png");
+document.querySelector("img").setAttribute("img","./images/dice6.png");
