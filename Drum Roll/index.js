10d9b117caae9ce30d5154b37ae5f582a8013c40
DITNO <92884446+DITNO@users.noreply.github.com>
2023-04-18 22:58:35 +0530

    Update index.js

--- Drum Roll/index.js
+++ Drum Roll/index.js
@@ -1,1 +1,10 @@
-alert('hii');
+var drumelement = document.querySelectorAll(".drum").length;
+
+for (var i =0;i<drumelement;i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener('click',handleclick);
+    
+    function handleclick(){
+        alert('I got clicked');
+    }
+}
