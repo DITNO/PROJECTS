495fc70024f3fb546db7645972d3cb2df5d2e211
DITNO <92884446+DITNO@users.noreply.github.com>
2023-04-21 13:12:57 +0530

    Update index.js

--- Drum Roll/index.js
+++ Drum Roll/index.js
@@ -7,35 +7,47 @@ for (var i =0;i<drumelement;i++){
     function handleclick(){
         
         var buttoninnerHTML = this.innerHTML;
-
-            switch (buttoninnerHTML) {
-                case ('w'):
-                    var audio_w = new Audio('./sounds/crash.mp3');
-                    audio_w.play();
-                    break;
-                case('a'):
-                    var audio_a = new Audio('./sounds/kick-bass.mp3');
-                    audio_a.play();
-                case('s'):
-                    var audio_s = new Audio('./sounds/snare.mp3');
-                    audio_s.play();
-                case('d'):
-                    var audio_d = new Audio('./sounds/tom-1.mp3');
-                    audio_d.play();
-                case('j'):
-                    var audio_j = new Audio('./sounds/tom-2.mp3');
-                    audio_j.play();
-                case('k'):
-                    var audio_k = new Audio('./sounds/tom-3.mp3');
-                    audio_k.play();
-                case('l'):
-                    var audio_l = new Audio('./sounds/tom-4.mp3');
-                    audio_l.play();
-            
-                default: console.log(buttoninnerHTML);
-                    break;
-            }
+        keycheck(buttoninnerHTML);
 
     }
 }
 
+document.addEventListener("keydown",function(event) {
+    var keypressuser = event.key;
+    keycheck(keypressuser);
+});
+
+function keycheck (key){
+    switch (key) {
+        case 'w':
+            var audio_w = new Audio('./sounds/crash.mp3');
+                    audio_w.play();
+            break;
+        case 'a':
+            var audio_a = new Audio('./sounds/kick-bass.mp3');
+                    audio_a.play();
+            break;
+        case's':
+            var audio_s = new Audio('./sounds/snare.mp3');
+            audio_s.play();
+            break;
+        case'd':
+            var audio_d = new Audio('./sounds/tom-1.mp3');
+            audio_d.play();
+            break;
+        case'j':
+            var audio_j = new Audio('./sounds/tom-2.mp3');
+            audio_j.play();
+            break;
+        case'k':
+            var audio_k = new Audio('./sounds/tom-3.mp3');
+            audio_k.play();
+            break;
+        case'l':
+            var audio_l = new Audio('./sounds/tom-4.mp3');
+            audio_l.play();
+            break;
+        default: console.log(keypressuser);
+            break;
+    }
+}
